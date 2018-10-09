459cf08c6b4c29c687b68da837a971896bc688b7
Lab User <LabUser@labs.salina.k-state.edu>
2018-10-09 12:11:21 -0500

    Committed bookinstance_list & populate

--- models/genre.js
+++ models/genre.js
@@ -2,11 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var GenreSchema = new Schema(
     {
-    title: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'Author', required: true},
-    summary: {type: String, required: true},
-    isbn: {type: String, required: true},
-    genre: [{type: Schema.Types.ObjectId, ref: 'Genre'}]
+    name: {type: String, required: true, min:3,max:100}
+   
   }
 );
     
